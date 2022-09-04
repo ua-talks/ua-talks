5090b8beff3d61fe3fa8184b36c4d37f4daf168e
Andrii Malyshko <pleasesendmeaspam@gmail.com>
2022-09-04 19:19:59 +0300

    Use Turbolinks

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -2,5 +2,7 @@ import "index.css"
 
 // Import all JavaScript & CSS files from src/_components
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
+import Turbolinks from "turbolinks"
 
+Turbolinks.start()
 console.info("Bridgetown is loaded!")
